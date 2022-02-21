ae909bbcc826921d78c8200d0c75301f44163597
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2022-02-22 02:56:25 +0530

    soretd marks and list of numbers

---
+++ pr9_marks_sort.py
@@ -0,0 +1,31 @@
+# sort
+
+m1 = int(input("Enter marks for student 1: "))
+m2 = int(input("Enter marks for student 2: "))
+m3 = int(input("Enter marks for student 3: "))
+m4 = int(input("Enter marks for student 4: "))
+m5 = int(input("Enter marks for student 5: "))
+m6 = int(input("Enter marks for student 6: "))
+
+marks_list = [m1,m2,m3,m4,m5,m6] 
+marks_list.sort()
+print(marks_list)
+
+# print(marks_list.sort())      # this will return None
+
+
+l1 = int(input("Enter list of numbers 1: "))
+l2 = int(input("Enter list of numbers 2: "))
+l3 = int(input("Enter list of numbers 3: "))
+l4 = int(input("Enter list of numbers 4: "))
+l5 = int(input("Enter list of numbers 5: "))
+l6 = int(input("Enter list of numbers 6: "))
+l7 = int(input("Enter list of numbers 7: "))
+l8 = int(input("Enter list of numbers 8: "))
+l9 = int(input("Enter list of numbers 9: "))
+list_numbers = [l1,l2,l3,l4,l5,l6,l7,l8,l9]
+
+list_numbers.sort()
+print(list_numbers)
+
+# Day 8
