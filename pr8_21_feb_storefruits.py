028a96d7eb3dbc15ecdfe677df85c903d295d084
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2022-02-21 18:04:31 +0530

    list stored enter by user

---
+++ pr8_21_feb_storefruits.py
@@ -0,0 +1,23 @@
+f1 = input("Enter Fruit Number 1: ")
+f2 = input("Enter Fruit Number 2: ")
+f3 = input("Enter Fruit Number 3: ")
+f4 = input("Enter Fruit Number 4: ")
+f5 = input("Enter Fruit Number 5: ")
+f6 = input("Enter Fruit Number 6: ")
+f7 = input("Enter Fruit Number 7: ")
+
+fruit_list = [f1,f2,f3,f4,f5,f6,f7]
+print(fruit_list)
+
+l1 = input("Enter language name 1: ")
+l2 = input("Enter language name 2: ")
+l3 = input("Enter language name 3: ")
+l4 = input("Enter language name 4: ")
+l5 = input("Enter language name 5: ")
+l6 = input("Enter language name 6: ")
+l7 = input("Enter language name 7: ")
+
+language = [l1,l2,l3,l4,l5,l6,l7]
+print(language)
+
+# Day 8
