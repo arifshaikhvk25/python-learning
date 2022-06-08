08d0e7565d5649a911a4fc07d15db35053f62da2
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2022-06-08 17:51:21 +0530

    find the word twinkle present in file pr34_poem.txt

---
+++ pr33_file_poemtxt_twinkle.py
@@ -0,0 +1,22 @@
+# ## find the word twinkle present in file pr34_poem.txt
+
+##################################################################################
+
+# find = open("pr34_poem.txt")
+# f = find.read()
+# if "twinkle" in f:
+#     print("twinkle is present in the file")
+# else:
+#     print("twinkle is not present in the file")
+# find.close()
+
+
+# f = open("poem.txt")
+# t = f.read()
+# if "twinkle" in t:
+#     print("twinkle is present in the file")
+# else:
+#     print("twinkle is not present in the file")
+# f.close()
+
+##################################################################################
